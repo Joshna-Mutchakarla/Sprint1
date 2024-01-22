ed7c021336c37245331d838c71e19651f59f056b
Joshna-Mutchakarla <101091267+Joshna-Mutchakarla@users.noreply.github.com>
2024-01-22 12:26:19 +0530

    Update Employee.java

--- EmployeeManagementSystem/src/main/java/com/anp/project/Employee.java
+++ EmployeeManagementSystem/src/main/java/com/anp/project/Employee.java
@@ -1,19 +1,24 @@
 package com.anp.project;
 
 
-	import javax.persistence.Column;
-	import javax.persistence.Entity;
-	import javax.persistence.GeneratedValue;
-	import javax.persistence.GenerationType;
-	import javax.persistence.Id;
-	import javax.persistence.Table;
+import javax.persistence.Column;// pre-defined package in java
+import javax.persistence.Entity;// pre-defined package in java
+import javax.persistence.GeneratedValue;// pre-defined package in java
+import javax.persistence.GenerationType;// pre-defined package in java
+import javax.persistence.Id;// pre-defined package in java
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;// pre-defined package in java
 	
 	
-	
+	// The @Entity annotation indicates that this class is a JPA entity and will be mapped to a database table.
 	@Entity
 	public class Employee {
-	@Id
+
+	@Id 
+	  // The @GeneratedValue annotation configures the way of incrementing the value of the annotated field.
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	// The @Column annotation is used to map the entity's field to the corresponding database column.
 	 @Column(name = "empId")
 	private int empId;
 	private String empFirstName;
@@ -22,6 +27,11 @@ package com.anp.project;
 	private String empEmail;
 	private long mobileNumber;
 	
+	@ManyToOne
+    @JoinColumn(name = "deptId")
+    private Department department;
+	
+	// Parameterized-Constructor
 	public Employee(int empId, String empFirstName, String empLastName, 
 	String empGender, String empEmail,
 	long mobileNumber) {
@@ -32,51 +42,76 @@ package com.anp.project;
 	this.empEmail = empEmail;
 	this.mobileNumber = mobileNumber;
 	}
+	// Zero-Argument-Constructor
 	public Employee() {
 	}
+	
+	// Getters and Setters
+	// Getter method for Employee Id 
 	public int getEmpId() {
 	return empId;
 	}
+	// Setter method for Employee Id 
 	public void setEmpId(int empId) {
 	this.empId = empId;
 	}
+	// Getter method for Employee FirstName
 	public String getEmpFirstName() {
 	return empFirstName;
 	}
+	// Setter method for Employee FirstName
 	public void setEmpFirstName(String empFirstName) {
 	this.empFirstName = empFirstName;
 	}
+	// Getter method for Employee LastName
 	public String getEmpLastName() {
 	return empLastName;
 	}
+	// Setter method for Employee LastName
 	public void setEmpLastName(String empLastName) {
 	this.empLastName = empLastName;
 	}
+	// Getter method for Employee Gender
 	public String getEmpGender() {
 	return empGender;
 	}
+	// Setter method for Employee Gender
 	public void setEmpGender(String empGender) {
 	this.empGender = empGender;
 	}
+	// Getter method for Employee Email
 	public String getEmpEmail() {
 	return empEmail;
 	}
+	// Setter method for Employee Email
 	public void setEmpEmail(String empEmail) {
 	this.empEmail = empEmail;
 	}
+	// Getter method for Employee MobileNumber
 	public long getMobileNumber() {
 	return mobileNumber;
 	}
+	// Setter method for Employee MobileNumber
 	public void setMobileNumber(long mobileNumber) {
 	this.mobileNumber = mobileNumber;
 	}
-	@Override
-	public String toString() {
-	return "Employee [empId=" + empId + ", empFirstName=" + 
-	empFirstName + ", empLastName=" + empLastName
-	+ ", empGender=" + empGender + ", empEmail=" + 
-	empEmail + ", mobileNumber=" + mobileNumber + "]";
-	}
+	
+	
+	 public Department getDepartment() {
+	        return department;
+	    }
+
+	    public void setDepartment(Department department) {
+	        this.department = department;
+	    }
+	//To string method
+		@Override
+		public String toString() {
+			return "Employee [empId=" + empId + ", empFirstName=" + empFirstName + ", empLastName=" + empLastName
+					+ ", empGender=" + empGender + ", empEmail=" + empEmail + ", mobileNumber=" + mobileNumber
+					+ ", department=" + department + "]";
+		}
+		
 	}
 
 
