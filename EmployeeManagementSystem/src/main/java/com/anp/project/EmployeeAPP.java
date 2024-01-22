9cf8ee30411e6d3acaa15a20b7649280220aafed
Joshna-Mutchakarla <101091267+Joshna-Mutchakarla@users.noreply.github.com>
2024-01-22 12:25:25 +0530

    Update EmployeeAPP.java

--- EmployeeManagementSystem/src/main/java/com/anp/project/EmployeeAPP.java
+++ EmployeeManagementSystem/src/main/java/com/anp/project/EmployeeAPP.java
@@ -3,19 +3,36 @@ package com.anp.project;
 public class EmployeeAPP {
 	public static void main(String[] args) {
 		 EmployeeDAO employeeDAO = new EmployeeDAO();
+		 DepartmentDAO departmentDAO = new DepartmentDAO();
+		 
+		  try {
+		 // Create and save a department
+	        Department itDepartment = new Department();
+	        itDepartment.setDeptName("IT Department");
+	        departmentDAO.saveDepartment(itDepartment);
 		 // Create and save an employee
 		 Employee newEmployee1 = new Employee(1, "John", "Doe", "Male", 
 		"john.doe@example.com", 1234567890);
+		 newEmployee1.setDepartment(itDepartment);
+		 
 		 Employee newEmployee2 = new Employee(2, "Alia", "Bhatt", 
 		"Female", "alia.bhatt@example.com", 1234567891);
+		 newEmployee2.setDepartment(itDepartment);
+		 
 		 Employee newEmployee3 = new Employee(3, "Varun", "Dhawan", 
 		"Male", "varun.dhawan@example.com", 1234567892);
-		 employeeDAO.saveEmployee(newEmployee1);
-		 employeeDAO.saveEmployee(newEmployee2);
-		 employeeDAO.saveEmployee(newEmployee3);
+		 newEmployee3.setDepartment(itDepartment);
+		 
+		 employeeDAO.saveEmployee(newEmployee1,itDepartment);
+		 employeeDAO.saveEmployee(newEmployee2,itDepartment);
+		 employeeDAO.saveEmployee(newEmployee3,itDepartment);
 		 // Fetch and display all employees
-		 System.out.println("All Employees: ");
-		 employeeDAO.getAllEmployees().forEach(System.out::println);
+		 System.out.println("All Employees with Departments: ");
+		 employeeDAO.getAllEmployees().forEach(employee -> {
+	            System.out.println(employee);
+	            System.out.println("Department: " + employee.getDepartment());
+	        });
+		
 		 // Update an employee
 		 Employee employeeToUpdate = employeeDAO.getEmployeeById(3);
 		 if (employeeToUpdate != null) {
@@ -32,6 +49,12 @@ public class EmployeeAPP {
 		 employeeDAO.getAllEmployees().forEach(System.out::println);
 		 // Close EntityManagerFactory
 		 employeeDAO.closeEntityManagerFactory();
-		 }
+		 } finally {
+	            // Close EntityManagerFactory
+	            employeeDAO.closeEntityManagerFactory();
+	            departmentDAO.closeEntityManagerFactory();
+	        }
+	}
 		}
+
 
